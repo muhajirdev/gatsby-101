a840da5ff9d8b2287e24f6b99ba98985bbbfa890
Muhammad Muhajir <muhajirframe@gmail.com>
2019-06-24 21:31:45 +0700

    sendgrid

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,17 +1,34 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+// import * as sendgrid from "@sendgrid/mail";
+
+// const SENDGRID_KEY = functions.config().sendgrid.key;
+// sendgrid.setApiKey(SENDGRID_KEY);
 
 admin.initializeApp();
 
 import { getContract, getCostumer } from "./billwerk";
+import {
+  addRecipients,
+  addRecipientsToList,
+  SUBSCRIBER_LIST_ID,
+} from "./sendgrid";
 
-const addSubscriber = (email: string) =>
+interface ISubscriber {
+  firstName: string;
+  lastName: string;
+  email: string;
+}
+
+const addSubscriber = (subscriber: ISubscriber) =>
   admin
     .firestore()
     .collection("subscribers")
-    .doc(email)
+    .doc(subscriber.email)
     .set({
-      allowed: true
+      allowed: true,
+      firstName: subscriber.firstName,
+      lastName: subscriber.lastName
     })
     .catch(e => {
       console.error(e);
@@ -23,8 +40,22 @@ export const orderSucceeded = functions.https.onRequest((request, response) => {
   getContract(ContractId)
     .then(contract => contract.CustomerId)
     .then(getCostumer)
-    .then(costumer => costumer.EmailAddress)
-    .then(addSubscriber)
+    .then(async costumer => {
+      await addSubscriber({
+        email: costumer.EmailAddress,
+        firstName: costumer.FirstName,
+        lastName: costumer.LastName
+      });
+      return addRecipients([
+        {
+          email: costumer.EmailAddress,
+          first_name: costumer.FirstName,
+          last_name: costumer.LastName
+        }
+      ]).then(recipients =>
+        addRecipientsToList(recipients, SUBSCRIBER_LIST_ID)
+      );
+    })
     .then(_ => {
       console.log("ok");
       response.send("ok");
